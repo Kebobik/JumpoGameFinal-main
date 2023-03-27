26671a743630730ad7fd914788e9a36377ea7337
Kebobik <kubamalina278@gmail.com>
2023-03-27 23:14:24 +0200

    background

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -12,10 +12,9 @@ public class CameraShake : MonoBehaviour
      public IEnumerator StartShake()
     {
         PostProccesing.SetActive(true);
-        vCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 5;
-        vCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = 5;
-        yield return new WaitForSeconds(1f);
-        
+        vCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 4;
+        vCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = 4;
+        yield return new WaitForSeconds(1f); 
     }
     public IEnumerator StopShake()
     {
